e6ad0988c73148a20795cb057c37f27fa2e6e34d
rehanmum0@gmail.com <rehanmum0@gmail.com>
2021-06-06 01:52:25 +0500

    some more

--- Frontend/src/components/Contact.js
+++ Frontend/src/components/Contact.js
@@ -3,16 +3,16 @@ import { Icon } from "rsuite";
 import "./Contact.css"
 const Contact = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container">
       <div className="d-flex flex-wrap justify-content-around">
         <div className="p-5 mb-2 bg-primary ">NUMBER 1</div>
         <div className="p-5 mb-2 bg-warning ">NUMBER 2</div>
         <div className="p-5 mb-2 bg-primary">NUMBER 3</div>
       </div>
 
-      <div className="container-fluid" >
-        <div className="row align-items-start">
-          <div className="col-10 offset-lg-1 d-flex flex-wrap justify-content-between ">
+      <div className="container" >
+        <div className="row">
+          <div className="col-lg-12 d-flex flex-wrap justify-content-between ">
             <div className="cont_height d-flex justify-content-start align-items-center rounded" style={{backgroundColor:"whitesmoke"}}>
               <Icon icon="phone-square" size="2x" className="ml-3 text-danger" />
               <div className="p-3">
@@ -37,6 +37,9 @@ const Contact = () => {
           </div>
         </div>
       </div>
+ {/* form */}
+    
+
     </div>
   );
 };
