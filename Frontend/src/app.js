ce9a2f99ab8ca5abc0bc015d199529abb62b463a
rehanmum0@gmail.com <rehanmum0@gmail.com>
2021-06-08 02:14:36 +0500

    ji

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Navbar from "./components/navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import About from "./components/About";
 import Login from "./components/login";
 import Signup from "./components/signup";
@@ -29,6 +30,7 @@ function App() {
               <Login />
             </Route>
           </Switch>
+          <Footer />
         </div>
       </Router>
     </>
--- Frontend/src/components/login.js
+++ Frontend/src/components/login.js
@@ -1,45 +1,60 @@
 import React from "react";
-import { Button, Form } from "react-bootstrap";
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 
-const login = () => {
-  // const [name, useName] = useState("");
-  // const [email, useEmail] = useState("");
-  // const [passwd, usePasswd] = useState("");
+const FormPage = () => {
+return (
 
-  return (
-    <div className="container">
-      <Form>
-        <Form.Group className="mb-3 mt-4" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            name="email"
-            id="email"
-            autoComplete="off"
-            placeholder="Enter email"
-            type="email"
-          />
-   
-        </Form.Group>
+<div class="card booking-card">
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            id="password"
-            autoComplete="off"
-            placeholder="Password"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </div>
-  );
+  {/* <!-- Card image --> */}
+  <div class="view overlay">
+    <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Food/8-col/img (5).jpg" alt="Card image cap">
+    <a href="#!">
+      <div class="mask rgba-white-slight"></div>
+    </a>
+  </div>
+
+  {/* <!-- Card content --> */}
+  <div class="card-body">
+
+    {/* <!-- Title --> */}
+    <h4 class="card-title font-weight-bold"><a>La Sirena restaurant</a></h4>
+    {/* <!-- Data --> */}
+    <ul class="list-unstyled list-inline rating mb-0">
+      <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"> </i></li>
+      <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+      <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+      <li class="list-inline-item mr-0"><i class="fas fa-star amber-text"></i></li>
+      <li class="list-inline-item"><i class="fas fa-star-half-alt amber-text"></i></li>
+      <li class="list-inline-item"><p class="text-muted">4.5 (413)</p></li>
+    </ul>
+    <p class="mb-2">$ • American, Restaurant</p>
+    {/* <!-- Text --> */}
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <hr class="my-4">
+    <p class="lead"><strong>Tonight's availability</strong></p>
+    <ul class="list-unstyled list-inline d-flex justify-content-between mb-0">
+      <li class="list-inline-item mr-0">
+        <div class="chip mr-0">5:30PM</div>
+      </li>
+      <li class="list-inline-item mr-0">
+        <div class="chip deep-purple white-text mr-0">7:30PM</div>
+      </li>
+      <li class="list-inline-item mr-0">
+        <div class="chip mr-0">8:00PM</div>
+      </li>
+      <li class="list-inline-item mr-0">
+        <div class="chip mr-0">9:00PM</div>
+      </li>
+    </ul>
+    {/* <!-- Button --> */}
+    <a href="#" class="btn btn-flat deep-purple-text p-1 mx-0 mb-0">Button</a>
+
+  </div>
+
+</div>
+
+);
 };
 
-export default login;
+export default FormPage;
